432ba91856477ee20168a0e80d9e84087a899641
Udeepa Sandakal <udeepasd@gmail.com>
2024-05-12 16:35:17 +0530

    card-compo-passing-whole-data-method01

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,25 +3,25 @@ import Star from "../assets/star.png";
 
 function Card(props) {
   let badgeText;
-  if (props.openSpots === 0) {
+  if (props.item.openSpots === 0) {
     badgeText = "SOLD OUT";
-  } else if (props.location === "Online") {
+  } else if (props.item.location === "Online") {
     badgeText = "ONLINE";
   }
 
   return (
     <div className="card">
       {badgeText && <div className="card--badge">{badgeText}</div>}
-      <img src={`../src/assets/${props.img}`} className="card--img" />
+      <img src={`../src/assets/${props.item.coverImg}`} className="card--img" />
       <div className="card--status">
         <img src={Star} className="card--star" />
-        <span>{props.rating}</span>
-        <span className="gray">({props.reviewCount}).</span>
-        <span className="gray">{props.location}</span>
+        <span>{props.item.stats.rating}</span>
+        <span className="gray">({props.item.stats.reviewCount}).</span>
+        <span className="gray">{props.item.location}</span>
       </div>
-      <p className="card--title">{props.title}</p>
+      <p className="card--title">{props.item.title}</p>
       <p className="card--price">
-        <span className="bold">From {props.price} $</span>/ Person
+        <span className="bold">From {props.item.price} $</span>/ Person
       </p>
     </div>
   );
