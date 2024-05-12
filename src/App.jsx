d8bb6ab7eb5f48cd2ab4fa10ffbf1c80e4d2a7da
Udeepa Sandakal <udeepasd@gmail.com>
2024-05-12 09:47:14 +0530

    passing-data-vis-props-card_components-done

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,27 @@ import React from "react";
 import Navbar from "./components/NavBar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
+import data from "./data.js";
 
 function App() {
+  const cards = data.map((item) => {
+    return (
+      <Card
+        img={item.coverImg}
+        rating={item.stats.rating}
+        reviewCount={item.stats.reviewCount}
+        country={item.location}
+        title={item.title}
+        price={item.price}
+      />
+    );
+  });
+
   return (
     <div>
       <Navbar />
       {/* <Hero /> */}
-      <Card />
+      <section className="card--list">{cards}</section>
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,19 @@
 import React from "react";
-import CardImg from "../assets/swimming.png";
 import Star from "../assets/star.png";
 
-function Card() {
+function Card(props) {
   return (
     <div className="card">
-      <img src={CardImg} className="card--img" />
+      <img src={`../src/assets/${props.img}`} className="card--img" />
       <div className="card--status">
         <img src={Star} className="card--star" />
-        <span>5.0</span>
-        <span className="gray">(6).</span>
-        <span className="gray">USA</span>
+        <span>{props.rating}</span>
+        <span className="gray">({props.reviewCount}).</span>
+        <span className="gray">{props.country}</span>
       </div>
-      <p className="">Life lessons</p>
+      <p className="">{props.title}</p>
       <p>
-        <span className="bold">From $136 </span>/ Person
+        <span className="bold">From {props.price} $</span>/ Person
       </p>
     </div>
   );
