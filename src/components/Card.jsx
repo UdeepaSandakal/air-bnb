01f493461bede8b0109e269483bb40bb58e34081
Udeepa Sandakal <udeepasd@gmail.com>
2024-05-12 16:26:00 +0530

    card-compo-done-with-normall-data passing

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,17 +2,25 @@ import React from "react";
 import Star from "../assets/star.png";
 
 function Card(props) {
+  let badgeText;
+  if (props.openSpots === 0) {
+    badgeText = "SOLD OUT";
+  } else if (props.location === "Online") {
+    badgeText = "ONLINE";
+  }
+
   return (
     <div className="card">
+      {badgeText && <div className="card--badge">{badgeText}</div>}
       <img src={`../src/assets/${props.img}`} className="card--img" />
       <div className="card--status">
         <img src={Star} className="card--star" />
         <span>{props.rating}</span>
         <span className="gray">({props.reviewCount}).</span>
-        <span className="gray">{props.country}</span>
+        <span className="gray">{props.location}</span>
       </div>
-      <p className="">{props.title}</p>
-      <p>
+      <p className="card--title">{props.title}</p>
+      <p className="card--price">
         <span className="bold">From {props.price} $</span>/ Person
       </p>
     </div>
